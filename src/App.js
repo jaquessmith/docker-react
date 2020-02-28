cfb1184ad0b5dd226ef98a3e3479dd66d088c4cf
Jaques <jaques.smith@gmail.com>
2020-02-28 15:42:56 +0200

    Create feature branch and changes app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br/>
-          Hi there Jaques
+          Hi there Jaques, this is the feature branch
         </p>
         <a
           className="App-link"
